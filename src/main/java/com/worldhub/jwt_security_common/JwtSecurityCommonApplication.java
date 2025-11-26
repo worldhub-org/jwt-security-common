907a481582227084bb4e68dedceb4c0694c63f1c
Viktor Aleksandrov <viktorostrov07@gmail.com>
2025-11-26 21:00:14 +0200

    initial commit

---
+++ src/main/java/com/worldhub/jwt_security_common/JwtSecurityCommonApplication.java
@@ -0,0 +1,13 @@
+package com.worldhub.jwt_security_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityCommonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worldhub/jwt_security_common/JwtSecurityCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldhub.jwt_security_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
