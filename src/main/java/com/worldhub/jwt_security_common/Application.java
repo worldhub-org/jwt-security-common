392303dc61087ec04262442382b5f3cdb6da3dd2
Viktor Aleksandrov <viktorostrov07@gmail.com>
2025-11-26 21:05:39 +0200

    initial commit

--- src/main/java/com/worldhub/jwt_security_common/Application.java
+++ src/main/java/com/worldhub/jwt_security_common/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JwtSecurityCommonApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JwtSecurityCommonApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
